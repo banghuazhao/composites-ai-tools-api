110cea045afa1845d912925922569950a39ff052
Banghua Zhao <banghua.zhao@gmail.com>
2025-02-25 09:41:10 +1300

    Add CORS configuration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.routers.v1.lamina_engineering_constants import router as lamina_engineering_constants_router_v1
@@ -8,6 +9,19 @@ from app.routers.v1.UDFRC_properties import router as udfrc_properties_router_v1
 from app.routers.v1.cylindrical_bending_analysis import router as cylindrical_bending_analysis_router_v1
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5000",               # Allow requests from your local development domain
+    "https://compositesai.com",    # Replace with your actual production domain
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,              # List of allowed origins
+    allow_credentials=True,             # Allow cookies, authorization headers, etc.
+    allow_methods=["*"],                # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],                # Allow all headers
+)
 
 app.mount("/results", StaticFiles(directory="results"), name="results")
 
